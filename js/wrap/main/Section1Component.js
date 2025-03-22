61af3149db78e9f1c978bfb224312dbe16d2884b
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2025-03-23 00:28:11 +0900

    c

---
+++ js/wrap/main/Section1Component.js
@@ -0,0 +1,14 @@
+// 내보내기 1개 
+export default {
+    template: 
+    `
+    <section id='section1'>
+        <h1>{{title}}</h1>
+    </section>
+    `, 
+    data() {
+        return {
+            title: 'Section1Component'
+        }
+    }
+}
